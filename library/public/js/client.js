965a7ae44c65e00f3e321b0debdebccf5edea8da
Jeff Kealy <jeffkealy@MacBook-Pro-2.local>
2016-11-14 08:32:54 -0600

    final commmit. Done.

--- library/public/js/client.js
+++ library/public/js/client.js
@@ -1,20 +1,25 @@
 $(document).ready(function(){
-  console.log("jquery working");
   $('.button').on('click', buttonClick);
-
-
 });
   function buttonClick(){
     var inputValue1 = $('#firstNum').val();
-    var inputValue2 = $('#secondNum').val();
     var buttonValue = $(this).attr('name');
     var valuesObj = {
       firstNumber: inputValue1,
-      secondNumber: inputValue2,
+      secondNumber: "",
       opperator: buttonValue,
+
     }
+    $('.button').css('background-color', 'lightGrey')
+    $(this).css('background-color', 'DimGrey')
+    console.log("obejct before equals click: ", valuesObj);
+    $('#equalsClick').on('click', function(){
+    valuesObj.secondNumber = $('#secondNum').val();
+
     sendMathData(valuesObj);
+    });
   }
+
 
     function sendMathData(valuesObj){
       $.ajax({
@@ -22,8 +27,7 @@ $(document).ready(function(){
         url: '/calculations',
         data: valuesObj,
         success: function(thisIsWhatIGetBack) {//the variable within the function is what the POST will send back
-          console.log("this is what i get back", thisIsWhatIGetBack);
-          console.log(valuesObj);
+          console.log("Posting this obect", valuesObj);
           getMathData();
 
         }
@@ -34,15 +38,21 @@ $(document).ready(function(){
         type: 'GET',
         url: '/calculations',
         success: function(thisIsWhatIGetBack){
-          console.log("got the get");
+          console.log("obect receieved from GET: ", thisIsWhatIGetBack);
           appendDom(thisIsWhatIGetBack);
+
+
         }
       })
     }
     function appendDom(thisIsWhatIGetBack) {
-        var dataFromPost = thisIsWhatIGetBack
+        var dataFromPost = thisIsWhatIGetBack;
         $('#result').empty();
-        $('#result').append('<div id="">' + dataFromPost.total + '</div>');
-        $('#firstNum').val("")
-        $('#secondNum').val("")
+        $('#result').append('<div id="resultId">' + dataFromPost.total + '</div>');
+        $('#firstNum').val("");
+        $('#secondNum').val("");
+        $('#resultId').append('<div id="clearButtonDiv"><button id="clearButton">Clear</button></div>');
+        $('#clearButton').on('click', function(){
+          $('#resultId').empty();
+        });
     }
